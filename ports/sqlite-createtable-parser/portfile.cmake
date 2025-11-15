5bba08eb36b042bb0d5740ad17a80764163c9bd2
Stephane Nolin <stephane.nolin@hotmail.com>
2025-11-15 13:51:22 -0500

    [sqlite-createtable-parser] Support the 2025-11-05 version

--- ports/sqlite-createtable-parser/portfile.cmake
+++ ports/sqlite-createtable-parser/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO            marcobambini/sqlite-createtable-parser
-	REF             368ffba73bfc221ff910a15455c6c27f9da5f20d
-	SHA512          9c4f261ed9b090da87ee6d9b349f38d06d9387451fbcab9fd16571023a5ebd37e24a8c47ee58bc206a3a1c20a2458ed5bee79549c57cf5527ddb1f7636cbe0c6
+	REF             e26e65b4b2bbe3aeccb007d5e78f0c880be32716
+	SHA512          78433fc339e8b0f39cc7dbd25812ebf9fc5ce324454df9661c54ce986fadd010419e841d1909a5a32dc69a5144222265b1849024b4c339464359f018505cea29
 	HEAD_REF        master
 )
 
