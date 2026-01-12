49e75176bfe97246e125771b77992264bb5a6055
Stephane Nolin <stephane.nolin@hotmail.com>
2026-01-12 03:53:55 -0500

    [sqlite-createtable-parser] Support the 2026-01-12 version

--- ports/sqlite-createtable-parser/portfile.cmake
+++ ports/sqlite-createtable-parser/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO            stephnol/sqlite-createtable-parser
-	REF             10779e7a92b453ca9c077e126931a2f73655780e
-	SHA512          c7d7f810ca684e5a3bb51a6198657f485d6e1d2df7d38f02f3d30c14be73f382dc23a11c0fa09a43137ec8636ca17627c7e1229a3895a738707f197fc2ba8f8a
-	HEAD_REF        feature/AUTOINCREMENT-CHECK-GENERATED
+	REPO            marcobambini/sqlite-createtable-parser
+	REF             4ae7652141fe2d7c62b2d0d546c14c4106037f26
+	SHA512          c9489ba9e5b4f662490bcc0b31070961e85f001f93f91cc07a206146bc9b75a228125216f7f2a5e74198149ed54ae72725144380499db13395ac6d19ebc11b8f
+	HEAD_REF        master
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"  DESTINATION "${SOURCE_PATH}")
