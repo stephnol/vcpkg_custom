3ca7c25645fb74ba59a26391f085e4c850b9c234
Stephane Nolin <stephane.nolin@hotmail.com>
2026-01-16 11:03:56 -0500

    [sqlite-createtable-parser] Support stephnol's 2026-01-16 version with: - Constraint name for every constraint type - Multiple check constraints

--- ports/sqlite-createtable-parser/portfile.cmake
+++ ports/sqlite-createtable-parser/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO            marcobambini/sqlite-createtable-parser
-	REF             4ae7652141fe2d7c62b2d0d546c14c4106037f26
-	SHA512          c9489ba9e5b4f662490bcc0b31070961e85f001f93f91cc07a206146bc9b75a228125216f7f2a5e74198149ed54ae72725144380499db13395ac6d19ebc11b8f
-	HEAD_REF        master
+	REPO            stephnol/sqlite-createtable-parser
+	REF             fe62c6c8060a14b445e1bf7b859e799f80a813c9
+	SHA512          751f73ef083c7a88473528f8bb5b0a69283cce398d827e396a25b5b10dc3c2b9eadbc0e681eb3f51f9908378f3c96c8785f9fd1abfeefa6aac3b89e6e2eceff8
+	HEAD_REF        feature/Support-Multiple-Check-Constraints
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"  DESTINATION "${SOURCE_PATH}")
